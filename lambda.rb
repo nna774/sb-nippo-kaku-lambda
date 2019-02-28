211d9f380c3daf61cbcb12e60417839d564e254f
Kugayama Nana <nonamea774@gmail.com>
2019-03-01 02:34:41 +0900

    only first time of the day

--- lambda.rb
+++ lambda.rb
@@ -1,13 +1,28 @@
 require 'date'
 require 'json'
 require 'uri'
+require 'aws-sdk-lambda'
+
+LASTORDER = ENV['lastOrder']
 
 def handler(event:, context:)
   project = ENV['PROJECT']    
   today = Date.today
   to = project + '/'
   to +=  "#{today.strftime('%Y%%2F%m%%2F%d')}"
-  body = "\n\n\n##{%w(日 月 火 水 木 金 土)[today.wday]}曜日 #nippo"
-  to += "?body=#{URI.encode(body)}"
+
+  client = Aws::Lambda::Client.new
+  res = client.invoke({
+    function_name: LASTORDER,
+    payload: {
+      name: 'sb-nippo-kaku-lambda'
+    }.to_json
+  })
+  result = JSON.parse(res.payload.string)
+  last = result['last']
+  if last.nil? || today.strftime('%D') != Time.at(last.to_i).strftime('%D')
+    body = "\n\n\n##{%w(日 月 火 水 木 金 土)[today.wday]}曜日 #nippo"
+    to += "?body=#{URI.encode(body)}"
+  end
   { location: to }
 end
