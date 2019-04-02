2f3439b69593bc7a4a4f6c3c08395313776a62c2
Kugayama Nana <nonamea774@gmail.com>
2019-04-02 17:16:44 +0900

    改行しすぎ。

--- lambda.rb
+++ lambda.rb
@@ -21,7 +21,7 @@ def handler(event:, context:)
   result = JSON.parse(res.payload.string)
   last = result['last']
   if last.nil? || today.strftime('%D') != Time.at(last.to_i).strftime('%D')
-    body = "\n\n\n##{%w(日 月 火 水 木 金 土)[today.wday]}曜日 #nippo"
+    body = "\n\n##{%w(日 月 火 水 木 金 土)[today.wday]}曜日 #nippo"
     to += "?body=#{URI.encode(body)}"
   end
   { location: to }
